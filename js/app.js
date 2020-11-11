666b4fb0984642428039e40e1a5d803484c841a7
nickdeans <nickabramowicz@gmail.com>
2020-11-10 18:30:55 -0800

    fixed js image randomizer and synchedwith repo

--- js/app.js
+++ js/app.js
@@ -5,17 +5,12 @@ var imageTwoElement = document.getElementById('image-two');
 var imageThreeElement = document.getElementById('image-three');
 var containerElement = document.getElementById('container');
 var voteCounterElement = document.getElementById('vote-counter');
-var totalClicks = 0;
-
-
-var firstRandomIndexNumber = 0;
-var secondRandomIndexNumber = 0;
-var thirdRandomIndexNumber = 0;
-
+var totalVotes = 0;
+var uniqueRandomNumbers = 0;
 var allProducts = [];
 
-var Product = function(name){
-    this.filePath = `imgs/${name}.jpg`;
+var Product = function(name, endOfFile){
+    this.filePath = `imgs/${name}.${endOfFile}`;
     this.title = this.alt = name;
     this.vote = 0;
     this.views = 0;
@@ -23,90 +18,87 @@ var Product = function(name){
     allProducts.push(this);
 }
 
-new Product('bag');
-new Product('banana');
-new Product('bathroom');
-new Product('boots');
-new Product('breakfast');
-new Product('bubblegum');
-new Product('chair');
-new Product('cthulhu');
-new Product('dog-duck');
-new Product('dragon');
-new Product('pen');
-new Product('pet-sweep');
-new Product('tauntaun');
-new Product('unicorn');
-new Product('usb');
-new Product('water-can');
-new Product('wine-glass');
-new Product('shark');
-new Product('sweep')
-new Product('scissors');
+new Product('bag', 'jpg');
+new Product('banana', 'jpg');
+new Product('bathroom', 'jpg');
+new Product('boots', 'jpg');
+new Product('breakfast', 'jpg');
+new Product('bubblegum', 'jpg');
+new Product('chair', 'jpg');
+new Product('cthulhu', 'jpg');
+new Product('dog-duck', 'jpg');
+new Product('dragon', 'jpg');
+new Product('pen', 'jpg');
+new Product('pet-sweep', 'jpg');
+new Product('tauntaun', 'jpg');
+new Product('unicorn', 'jpg');
+new Product('usb', 'gif');
+new Product('water-can', 'jpg');
+new Product('wine-glass', 'jpg');
+new Product('shark', 'jpg');
+new Product('sweep', 'png')
+new Product('scissors', 'jpg');
+
+
+var render = function(){
+    getUniqueRandomNumbers();
+
+    var firstRandomIndexNumber = uniqueRandomNumbers[0];
+    var secondRandomIndexNumber = uniqueRandomNumbers[1];
+    var thirdRandomIndexNumber = uniqueRandomNumbers[2];
+
+    imageOneElement.src = allProducts[firstRandomIndexNumber].filePath;
+    imageOneElement.title = allProducts[firstRandomIndexNumber].title;
+    imageOneElement.alt = allProducts[firstRandomIndexNumber].alt;
+    allProducts[firstRandomIndexNumber].views++;
+
+    imageTwoElement.src = allProducts[secondRandomIndexNumber].filePath;
+    imageTwoElement.title = allProducts[secondRandomIndexNumber].title;
+    imageTwoElement.alt = allProducts[secondRandomIndexNumber].alt;
+    allProducts[secondRandomIndexNumber].views++;
+
+    imageThreeElement.src = allProducts[thirdRandomIndexNumber].filePath;
+    imageThreeElement.title = allProducts[thirdRandomIndexNumber].title;
+    imageThreeElement.alt = allProducts[thirdRandomIndexNumber].alt;
+    allProducts[thirdRandomIndexNumber].views++;
+}
+
+function getUniqueRandomNumbers(){
+    uniqueRandomNumbers = [];
+
+    for(var i=0; i<3; i++){
+        var randomNumber = randomIndexGenerator();
+        while(uniqueRandomNumbers.includes(randomNumber)){
+            randomNumber = randomIndexGenerator;
+        }
+        uniqueRandomNumbers.unshift(randomNumber);
+    }
+    while(uniqueRandomNumbers.length > 3){
+        uniqueRandomNumbers.pop();
+    }
+}
 
 function randomIndexGenerator(){
     return Math.floor(Math.random() * allProducts.length);
 }
 
-function render(){
-    firstRandomIndexNumber = randomIndexGenerator();
-    secondRandomIndexNumber = randomIndexGenerator();
-    thirdRandomIndexNumber = randomIndexGenerator();
-
-    while(secondRandomIndexNumber === firstRandomIndexNumber){
-        secondRandomIndexNumber = randomIndexGenerator();
-
-    }
-
-    while(thirdRandomIndexNumber === secondRandomIndexNumber){
-        thirdRandomIndexNumber = randomIndexGenerator();
-
-    }
-
-    while(firstRandomIndexNumber === thirdRandomIndexNumber){
-        firstRandomIndexNumber = randomIndexGenerator();
-
-    }
-
-imageOneElement.src = allProducts[firstRandomIndexNumber].filePath;
-imageOneElement.title = allProducts[firstRandomIndexNumber].title;
-imageOneElement.alt = allProducts[firstRandomIndexNumber].alt;
-
-imageTwoElement.src = allProducts[secondRandomIndexNumber].filePath;
-imageTwoElement.title = allProducts[secondRandomIndexNumber].title;
-imageTwoElement.alt = allProducts[secondRandomIndexNumber].alt;
-
-imageThreeElement.src = allProducts[thirdRandomIndexNumber].filePath;
-imageThreeElement.title = allProducts[thirdRandomIndexNumber].title;
-imageThreeElement.alt = allProducts[thirdRandomIndexNumber].alt;
-
-}
-
 function handleClick(e){
-    totalClicks++;
-    if(totalClicks<=25){
-        var title = e.target.title;
-        for(var i=0; i<allProducts.length; i++){
-            if(allProducts[i].title === title){
-                allProducts[i].vote++;
-            }
-            render();
-    }  var title = e.target.title;
+    var titleClick = e.target.title;
     for(var i=0; i<allProducts.length; i++){
-        if(allProducts[i].title === title){
-            allProducts[i].views++;
+        if(titleClick === allProducts[i].title){
+            allProducts[i].vote++;
+            totalVotes++;
         }
     }
-    }else{
-    for(var i=0; i<allProducts.length; i++){
-        var liElement=document.createElement('li')
-        liElement.textContent=`${allProducts[i].title} was voted ${allProducts[i].vote} and was seen ${allProducts[i].views} times`;
-        voteCounterElement.appendChild(liElement)
+
+    render();
+
+    if(totalVotes === 25){
+        containerElement.removeEventListener('click', handleClick);
     }
-}
 }
 
 containerElement.addEventListener('click', handleClick);
-//
+
 render();
 
